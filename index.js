bc97eee42967489877f86116856561120d23786f
Arthur-Serafim <dev.seraph.arthur@gmail.com>
2019-12-15 23:56:09 -0300

    Finish basic transactions

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const http = require("http");
+const router = require("./router");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+const PORT = process.env.PORT | 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+io.on("connect", socket => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`
+    });
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `${user.name} has joined the room.`
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      socket.broadcast.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left the room.`
+      });
+    }
+  });
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ users.js
@@ -0,0 +1,36 @@
+let users = [];
+
+function addUser({ id, name, room }) {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingUser = users.find(
+    user => user.room === room && user.name === name
+  );
+
+  if (existingUser) {
+    return { error: "Username is taken" };
+  }
+
+  const user = { id, name, room };
+  users = [...users, user]
+
+  return { user };
+}
+
+function removeUser(id) {
+  user = users.find(user => user.id === id)
+  users = users.filter(user => user.id !== id);
+
+  return user;
+}
+
+function getUser(id) {
+  return users.find(user => user.id === id)
+}
+
+function getUsersInRoom(room) {
+  return users.filter(user => user.room === room);
+}
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
